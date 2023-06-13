0c7ce5cdf53754765bbf4f0c858bb75ac3530e85
Fernando Herman Pookey <nandopookey@gmail.com>
2023-06-14 06:40:18 +0700

    Buat authentikasi dan tampilan login, registrasi belum

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
+    // public const SUB = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'first_name'    => 'Admin',
+            'last_name'     => 'Gym',
+            'email'         => 'admin@gmail.com',
+            'roles'         => 'ADMIN',
+            'gender'        => 'Male',
+            'password'      => Hash::make('1')
+        ]);
+    }
+}
